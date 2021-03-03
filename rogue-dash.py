965fe9780737e010038711ce2989f03f240d784b
Christopher Capobianco <ccapo@users.noreply.github.com>
2021-03-03 02:09:10 -0500

    Add custom tiles, including randomly generated tiles

--- rogue-dash.py
+++ rogue-dash.py
@@ -1,5 +1,6 @@
 import tcod as libtcod
 
+from tiles import CharType
 from entity import Entity
 from handlers import handle_keys
 from map.map import Map
@@ -17,11 +18,289 @@ def main():
     'dark_ground': libtcod.Color(50, 50, 150)
   }
 
-  player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
-  npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', libtcod.yellow)
-  entities = [npc, player]
+  libtcod.console_set_custom_font('data/fonts/arial16x16-ext2.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD, 32, 46)
 
-  libtcod.console_set_custom_font('data/fonts/arial12x12.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+  # Assign extra ascii keys
+  x = -1
+  y = 8
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_STAIRS_UP), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_STAIRS_DOWN), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_HOLE), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_WATER_01), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_WATER_02), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_LAVA_01), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_LAVA_02), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_CHEST_OPEN), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_CHEST_CLOSED), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_POTION_YELLOW), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_POTION_RED), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_POTION_GREEN), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_POTION_BLUE), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_KEY), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_RING_RED), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_RING_GREEN), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_RING_BLUE), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_RING_RED_BIG), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_RING_GREEN_BIG), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_RING_BLUE_BIG), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SHIELD_BROWN), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SHIELD_GREY), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SHIELD_GOLD), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SWORD_BASIC), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SWORD_STEEL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SWORD_GOLD), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_ARMOUR_BROWN), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_ARMOUR_YELLOW), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_ARMOUR_RED), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_ARMOUR_GREEN), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_ARMOUR_BLUE), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_ARMOUR_MITHRIL), x, y)
+  x = -1
+  y += 1
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_CHARGEBAR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_PLAYER_RIGHT), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_PLAYER_DOWN), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_PLAYER_LEFT), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_PLAYER_UP), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_GUARDIAN), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_KEEPER), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_PERSON_MALE), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_PERSON_FEMALE), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_GUARD), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_WARLOCK_PURPLE), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_NECROMANCER_APPENTICE), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_NECROMANCER_MASTER), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DARKELF_ARCHER), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DARKELF_WARRIOR), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DARKELF_MAGE), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DWARF_WARRIOR), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DWARF_AXEBEARER), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DWARF_MAGE), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DWARF_HERO), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_UNDEAD_DWARF_WARRIOR), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_UNDEAD_DWARF_AXEBEARER), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_UNDEAD_DWARF_MAGE), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_UNDEAD_DWARF_HERO), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_GOBLIN_PEON), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_GOBLIN_WARRIOR), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_GOBLIN_MAGE), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_IMP_BLUE), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_IMP_RED), x, y)
+  x = -1
+  y += 1
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_ORGE_PEON_GREEN), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_ORGE_WARRIOR_GREEN), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_ORGE_PEON_RED), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_ORGE_WARRIOR_RED), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SKELETON_PEON), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SKELETON_WARRIOR), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SKELETON_HERO), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SKELETON_MAGE), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SPRITE), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_ORC_PEON), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_ORC_WARRIOR), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_ORC_HERO), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_ORC_MAGE), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DEMON_PEON), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DEMON_HERO), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DEMON_MAGE), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_FLAYER_WARRIOR), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_FLAYER_MAGE), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SKULL), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_GOLEM_GREY), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_GOLEM_BROWN), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_GOLEM_RED), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SLIME_BROWN), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SLIME_GREEN), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_EYEBALL), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_VERMIN_BROWN), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SNAKE_GREEN), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_RUBBLE_PILE), x, y)
+  x = -1
+  y += 1
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SCORPIAN_YELLOW), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SCORPIAN_BLACK), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SPIDER_BLACK), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SPIDER_RED), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SPIDER_GREEN), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_PYTHON_RED), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_PYTHON_BROWN), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_PYTHON_YELLOW), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_BAT_BROWN), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_BAT_BLUE), x, y)
+
+  # Environment Tiles
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_TREE_A), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_TREE_B), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_TREE_C), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_TREE_D), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_TREE_E), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_TREE_F), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_TREE_G), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SHRUB_A), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SHRUB_B), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SHRUB_C), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SHRUB_D), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_MUSHROOM), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_FLOWERS_WHITE), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_FLOWERS_BLUE), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_TEMPLE), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_TOWN), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_CAVE), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_BED), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_TABLE), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_BOOKCASE), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_CHAIR_RIGHT), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_CHAIR_LEFT), x, y)
+  x = -1
+  y += 1
+
+  # Minor Bosses (Upper Portion)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DEMONLORD_WHITE_UL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DEMONLORD_WHITE_UR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DEMONLORD_RED_UL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DEMONLORD_RED_UR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_CYCLOPS_UL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_CYCLOPS_UR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DRAGON_LARGE_RED_UL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DRAGON_LARGE_RED_UR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DRAGON_LARGE_YELLOW_UL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DRAGON_LARGE_YELLOW_UR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DRAGON_LARGE_GREEN_UL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DRAGON_LARGE_GREEN_UR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DRAGON_LARGE_BLUE_UL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DRAGON_LARGE_BLUE_UR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DRAGON_LARGE_GREY_UL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DRAGON_LARGE_GREY_UR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_MINOTAUR_UL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_MINOTAUR_UR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_LIZARD_GIANT_UL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_LIZARD_GIANT_UR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_MEDUSA_UL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_MEDUSA_UR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_FLYING_BRAIN_UL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_FLYING_BRAIN_UR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SLIMELORD_UL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SLIMELORD_UR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_BEHOLDER_UL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_BEHOLDER_UR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_BEHEMOTH_UL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_BEHEMOTH_UR), x, y)
+
+  # Final Boss (Upper Portion)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_FINAL_BOSS_UL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_FINAL_BOSS_UR), x, y)
+  x = -1
+  y += 1
+
+  # Minor Bosses (Lower Portion)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DEMONLORD_WHITE_LL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DEMONLORD_WHITE_LR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DEMONLORD_RED_LL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DEMONLORD_RED_LR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_CYCLOPS_LL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_CYCLOPS_LR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DRAGON_LARGE_RED_LL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DRAGON_LARGE_RED_LR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DRAGON_LARGE_YELLOW_LL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DRAGON_LARGE_YELLOW_LR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DRAGON_LARGE_GREEN_LL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DRAGON_LARGE_GREEN_LR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DRAGON_LARGE_BLUE_LL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DRAGON_LARGE_BLUE_LR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DRAGON_LARGE_GREY_LL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_DRAGON_LARGE_GREY_LR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_MINOTAUR_LL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_MINOTAUR_LR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_LIZARD_GIANT_LL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_LIZARD_GIANT_LR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_MEDUSA_LL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_MEDUSA_LR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_FLYING_BRAIN_LL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_FLYING_BRAIN_LR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SLIMELORD_LL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_SLIMELORD_LR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_BEHOLDER_LL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_BEHOLDER_LR), x, y)
+
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_BEHEMOTH_LL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_BEHEMOTH_LR), x, y)
+
+  # Final Boss (Lower Portion)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_FINAL_BOSS_LL), x, y)
+  x += 1; libtcod.console_map_ascii_code_to_font(int(CharType.CHAR_FINAL_BOSS_LR), x, y)
+
+  player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
+  npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), CharType.CHAR_KEEPER, libtcod.white)
+  entities = [npc, player]
 
   libtcod.console_init_root(screen_width, screen_height, 'rogue-dash (2021 7DRL)', False)
 
