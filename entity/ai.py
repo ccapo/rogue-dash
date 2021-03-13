fbc9137369211a6ab1e28763798e2fd08c7b4153
Christopher Capobianco <ccapo@users.noreply.github.com>
2021-03-13 02:27:11 -0500

    Fix player speed controlled movement

--- entity/ai.py
+++ entity/ai.py
@@ -18,27 +18,31 @@ class AI:
       exit = action.get('exit')
       fullscreen = action.get('fullscreen')
 
-      if move:
-        dx, dy = move
-        dest_x = owner.x + dx
-        dest_y = owner.y + dy
-        if not engine.map.is_blocked(dest_x, dest_y):
-          target = engine.get_entities(dest_x, dest_y)
-          if target is not None:
-            owner.attack(target)
-          else:
-            owner.move(dx, dy)
-            if dx == 0 and dy == -1:
-              owner.sym = CharType.PLAYER_UP
-            elif dx == -1 and dy == 0:
-              owner.sym = CharType.PLAYER_LEFT
-            elif dx == 0 and dy == 1:
-              owner.sym = CharType.PLAYER_DOWN
-            elif dx == 1 and dy == 0:
-              owner.sym = CharType.PLAYER_RIGHT
-            obj = engine.get_items(owner.x, owner.y)
-            if obj is not None:
-              obj.use()
+      if owner.stats.spd*self.threshold >= 1.0:
+        if move:
+          self.threshold = 0.0
+          dx, dy = move
+          dest_x = owner.x + dx
+          dest_y = owner.y + dy
+          if not engine.map.is_blocked(dest_x, dest_y):
+            target = engine.get_entities(dest_x, dest_y)
+            if target is not None:
+              owner.attack(target)
+            else:
+              owner.move(dx, dy)
+              if dx == 0 and dy == -1:
+                owner.sym = CharType.PLAYER_UP
+              elif dx == -1 and dy == 0:
+                owner.sym = CharType.PLAYER_LEFT
+              elif dx == 0 and dy == 1:
+                owner.sym = CharType.PLAYER_DOWN
+              elif dx == 1 and dy == 0:
+                owner.sym = CharType.PLAYER_RIGHT
+              obj = engine.get_items(owner.x, owner.y)
+              if obj is not None:
+                obj.use()
+      else:
+        self.threshold += libtcod.sys_get_last_frame_length()
 
       # If player is at bottom edge of visible map, push them up
       if owner.y > engine.map.camera_height + engine.map.camera_yoffset - 1:
