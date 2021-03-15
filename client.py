972e65c5573f1d41f5684ad5be12f5fc1d014477
Christopher Capobianco <ccapo@users.noreply.github.com>
2021-03-14 23:01:02 -0400

    Enable access to game server

--- client.py
+++ client.py
@@ -3,51 +3,50 @@ import requests
 import json
 
 class Client:
-  def __init__(self):
+  def __init__(self, host = 'https://rogue-dash.site'):
+    self.host = host
     self.credPath = "data/credentials.json"
-    self.urlReg = "http://localhost:8080/api/register"
-    self.urlAuth = "http://localhost:8080/api/authenticate"
-    self.urlScore = "http://localhost:8080/api/score"
+    self.urlReg = self.host + "/api/register"
+    self.urlAuth = self.host + "/api/authenticate"
+    self.urlScore = self.host + "/api/score"
 
   def upload_score(self, scorecard):
     file = pathlib.Path(self.credPath)
     credJSON = {}
     if file.exists():
       with open(self.credPath) as json_file:
-          credJSON = json.load(json_file)
+        credJSON = json.load(json_file)
     else:
       try:
-        r = requests.post(self.urlReg)
+        req = requests.post(self.urlReg)
       except:
         return
 
-      if r.status_code != requests.codes.ok:
-        print("Request #1 failed with code: {}".format(r.status_code))
+      if req.status_code != requests.codes.ok:
+        print("Registration request failed with code: {}".format(req.status_code))
         return
 
-      credJSON = r.json()
+      credJSON = req.json()
       with open(self.credPath, 'w') as outfile:
         json.dump(credJSON, outfile)
 
     try:
-      r2 = requests.post(self.urlAuth, data=credJSON)
+      req = requests.post(self.urlAuth, data=credJSON)
     except:
       return
 
-    if r2.status_code != requests.codes.ok:
-      print("Request #2 failed with code: {}".format(r2.status_code))
+    if req.status_code != requests.codes.ok:
+      print("Authentication request failed with code: {}".format(req.status_code))
       return
 
-    tokenJSON = r2.json()
+    tokenJSON = req.json()
     headers = {'Authorization': 'Bearer '+tokenJSON['accessToken'], 'Content-Type': 'application/json'}
 
     try:
-      r3 = requests.post(self.urlScore, headers=headers, data=json.dumps(scorecard))
+      req = requests.post(self.urlScore, headers=headers, data=json.dumps(scorecard))
     except:
       return
 
-    if r3.status_code != requests.codes.ok:
-      print("Request #3 failed with code: {}".format(r3.status_code))
+    if req.status_code != requests.codes.ok:
+      print("Score upload failed with code: {}".format(req.status_code))
       return
-
-    print(r3.json())
